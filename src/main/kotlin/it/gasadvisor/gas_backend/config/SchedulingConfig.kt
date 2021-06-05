212e5e51fb4653ed88c7e54567ae7e3ba123634e
Diego Cipa <diego@Diegos-MacBook-Pro.local>
2021-06-05 21:32:45 +0200

    added cron

---
+++ src/main/kotlin/it/gasadvisor/gas_backend/config/SchedulingConfig.kt
@@ -0,0 +1,8 @@
+package it.gasadvisor.gas_backend.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SchedulingConfig
--- src/main/kotlin/it/gasadvisor/gas_backend/job/priceupdate/scheduler/JobUpdateGasStationPrice.kt
+++ src/main/kotlin/it/gasadvisor/gas_backend/job/priceupdate/scheduler/JobUpdateGasStationPrice.kt
@@ -5,6 +5,7 @@ import it.gasadvisor.gas_backend.job.priceupdate.processor.PriceUpdateProcessor
 import it.gasadvisor.gas_backend.util.Log
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
@@ -14,6 +15,7 @@ class JobUpdateGasStationPrice @Autowired constructor(
 ) {
     companion object : Log()
 
+    @Scheduled(cron = "\${job.update.gas.cron}", zone = "Europe/Rome")
     fun init() {
         log.info("Starting price update job")
         stationPriceUpdateProcessor.update()
