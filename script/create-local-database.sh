33dca78c05224cf9aa554c5bbdbe1f3a93a276af
diego <diegocipa@ymail.com>
2021-11-03 21:25:23 +0100

    Added liquibase update in db creatioN

--- script/create-local-database.sh
+++ script/create-local-database.sh
@@ -20,8 +20,8 @@ function do_mysql() {
 
 function create_db() {
   db=$1
-  #changeLogFile=$2
-  seed=$2
+  changeLogFile=$2
+  seed=$3
 
   echo "drop database if exists $db" | do_mysql
   echo "create database $db" | do_mysql
@@ -31,6 +31,16 @@ function create_db() {
   echo "grant all on $db.* to '$LOCAL_DB_USER'@'%' identified by '$LOCAL_DB_PASSWORD';" | do_mysql
   echo "grant all on $db.* to '$LOCAL_DB_USER'@'$LOCAL_DB_HOST' identified by '$LOCAL_DB_PASSWORD';" | do_mysql
   echo "PERMISSIONS GRANTED FOR USER '$LOCAL_DB_USER' AND DATABASE '$db' "
+
+  java -jar liquibase/lib/liquibase-core-3.5.3.jar \
+    --changeLogFile="$changeLogFile" \
+    --username="$LOCAL_DB_USER" \
+    --password="$LOCAL_DB_PASSWORD" \
+    --url="jdbc:mysql://$LOCAL_DB_HOST:3306/$db?useSSL=false" \
+    --classpath="liquibase/lib/mysql-connector-java-5.1.38.jar" \
+    --driver="com.mysql.jdbc.Driver" \
+    --logLevel="warning" \
+    update
 
   if [ "$seed" != "" ]; then
     echo " loading seed data..."
@@ -45,5 +55,5 @@ if [ "$MYSQL_PWD" == "" ]; then
   echo ""
 fi
 
-create_db $LOCAL_DB_SCHEMA liquibase/seed.sql
-create_db ${LOCAL_DB_SCHEMA}_test liquibase/seed.sql
+create_db $LOCAL_DB_SCHEMA liquibase/changeLog-master.xml liquibase/seed.sql
+create_db ${LOCAL_DB_SCHEMA}_test liquibase/changeLog-master.xml liquibase/seed.sql
